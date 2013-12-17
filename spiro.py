8807e4a7b46f66297231abd1fbb557bddeaa776d
Lauri Hakko <lauri.hakko@gmail.com>
2013-12-17 12:14:47 +0200

    works now

--- spiro.py
+++ spiro.py
@@ -5,25 +5,23 @@ from PIL import Image, ImageDraw
 
 class line(object):
 
-    def __init__(self, direction=1, length=1, speed=1):
-        self.direction = direction
+    def __init__(self, length=1, speed=1):
         self.length = length
         self.speed = speed
         self.rotation = 0
 
     def step(self):
-        self.rotation += self.direction * self.speed
+        self.rotation += self.speed
 
-    def delta(self):
+    def get_vector(self):
         rads = pi * self.rotation / 360
         x = self.length * cos(rads)
         y = self.length * sin(rads)
         return x, y
 
 spiro = []
-spiro.append(line(length=100))
-spiro.append(line(length=40, speed=10))
-spiro.append(line(length=30))
+spiro.append(line(length=100, speed=7))
+spiro.append(line(length=40, speed=17))
 
 spirolen = 0
 for i in spiro:
@@ -34,15 +32,15 @@ center = [spirolen + 10, spirolen + 10]
 
 im = Image.new("RGB", (size, size))
 draw = ImageDraw.Draw(im)
-prev = center
+prev = center[:]
 prev[0] += spirolen
-for i in range(100000):
-    new = center
+for i in range(10000):
+    new = center[:]
     for part in spiro:
         part.step()
-        delta = part.delta()
-        new[0] += delta[0]
-        new[1] += delta[1]
+        vector = part.get_vector()
+        new[0] += vector[0]
+        new[1] += vector[1]
     draw.line((tuple(prev), tuple(new)), fill=128)
     prev = new[:]
 del draw
